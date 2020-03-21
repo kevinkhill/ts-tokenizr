65515a7b611b80b70223eefd18172ba9dd1f2e2c
Kevin Hill <kevinkhill@gmail.com>
2020-03-21 07:38:30 -0700

    fixed the demo and added a dev version to hack on

--- demo/sample.ts
+++ demo/sample.ts
@@ -1,26 +1,44 @@
-import fs       from "fs"
-import path     from "path"
+import fs from "fs";
+import path from "path";
 
-import Tokenizr from "../src";liconstokenizr"
+import { Tokenizr } from "../src";
 
-let lexer = n;ew Tokenizr()
+const lexer = new Tokenizr({
+  debug: false
+});
 
-lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/, (ctx, mctx.accept("id");.ac;cept("id")
-})
-lexer.rule(/[+-]?[0-9]+/, (ctx,  ctx.accept("number", parseInt(match[0]));nt(;match[0]))
-})
-lexer.rule(/"((?:\\\"|[^\r\n])*)"/, (ctx, mctx.accept("string", match[1].replace(/\\"/g, '"'));\\";/g, "\""))
-})
-lexer.rule(/\/\/[^\r\n]*\r?\n/, (ctx,  ctx.ignore(); ct;x.ignore()
-})
-lexer.rule(/[ \t\r\n]+/, (ctx, mctx.ignore(); ct;x.ignore()
-})
-lexer.rule(/./, (ctx, mctx.accept("char");cce;ptconsthar")
-})
+// This still works, but I like config objects
+// lexer.debug(false);
 
-let cfg = fs.readFileSync(path.join(__dirname, "sample.c;fg"), "utf8")
+lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/, ctx => {
+  ctx.accept("id");
+});
 
-lex;er.input(cfg)
-lexer;.debug(false)
-lexer.toketokenrEach(  console.log(token.toString());n.t;oString())
-})
+lexer.rule(/[+-]?[0-9]+/, (ctx, match) => {
+  ctx.accept("number", parseInt(match[0]));
+});
+
+lexer.rule(/"((?:\\\"|[^\r\n])*)"/, (ctx, match) => {
+  ctx.accept("string", match[1].replace(/\\"/g, '"'));
+});
+
+lexer.rule(/\/\/[^\r\n]*\r?\n/, ctx => {
+  ctx.ignore();
+});
+
+lexer.rule(/[ \t\r\n]+/, ctx => {
+  ctx.ignore();
+});
+
+lexer.rule(/./, ctx => {
+  ctx.accept("char");
+});
+
+fs.promises
+  .readFile(path.join(__dirname, "sample.cfg"), "utf8")
+  .then(contents => {
+    lexer.input(contents);
+    lexer.tokens().forEach(token => {
+      console.log(token.toString());
+    });
+  });
